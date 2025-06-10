dd071f73e9487e884f336df718c3f81fe8538838
fabian.wilms <fabian.wilms@muenchen.de>
2025-06-10 07:25:58 +0200

    :pencil_2: english log output

--- cac-trigger-exporter.sh
+++ cac-trigger-exporter.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Stelle sicher, dass du dich zuvor per 'oc login' am Cluster angemeldet hast."
+echo "Make sure you're logged in to your Openshift-cluster first."
 
 # Überprüfen, ob der Trigger-Name als Argument übergeben wurde
 if [ "$#" -ne 2 ]; then
@@ -13,7 +13,7 @@ TRIGGER_NAME="$2"
 RAILS_POD_LABEL="app.kubernetes.io/component=zammad-railsserver"
 
 # Ins richtige Projekt wechseln
-oc project "$PROJECT" || { echo "❌ Konnte nicht zu Projekt $PROJECT wechseln."; exit 1; }
+oc project "$PROJECT" || { echo "❌ Couldn't change to project $PROJECT."; exit 1; }
 
 # Pod-Namen eines aktuellen Railsservers herausfinden
 RAILS_POD=$(oc get pods -l "$RAILS_POD_LABEL" -o name | head -n 1 | cut -d '/' -f 2)
@@ -23,8 +23,8 @@ if [ -z "$RAILS_POD" ]; then
 fi
 
 # Trigger extrahieren und in eine JSON-Datei schreiben
-echo "Versuche Trigger <$TRIGGER_NAME> zu laden..."
-oc rsh "$RAILS_POD" rails runner "require 'pp'; result = Trigger.find_by(name: '$TRIGGER_NAME').attributes; pp result if result" > rails_output.txt || { echo "❌ Konnte Trigger <$TRIGGER_NAME> nicht laden."; exit 1; }
+echo "Trying to load trigger <$TRIGGER_NAME> ..."
+oc rsh "$RAILS_POD" rails runner "require 'pp'; result = Trigger.find_by(name: '$TRIGGER_NAME').attributes; pp result if result" > rails_output.txt || { echo "❌ Couldn't load trigger <$TRIGGER_NAME>."; exit 1; }
 
 # Logausgaben (mit I... am Anfang) entfernen und in trigger.ruby schreiben
 grep -v '^I' rails_output.txt > trigger.ruby
@@ -40,7 +40,7 @@ if [[ $triggerWithoutWhitespace =~ $SOURCE_GROUP_ID_REGEX ]]
 then
     SOURCE_GROUP_ID="${BASH_REMATCH[1]}"
 else
-    echo "❌ Sourcegroup-ID konnte nicht ermittelt werden."
+    echo "❌ Sourcegroup-ID couldn't be evaluated."
     exit 1
 fi
 
@@ -48,16 +48,16 @@ if [[ $triggerWithoutWhitespace =~ $TARGET_GROUP_ID_REGEX ]]
 then
     TARGET_GROUP_ID="${BASH_REMATCH[1]}"
 else
-    echo "❌ Targetgroup-ID konnte nicht ermittelt werden."
+    echo "❌ Targetgroup-ID couldn't be evaluated."
     exit 1
 fi
 
-echo "Versuche Name von Sourcegroup <$SOURCE_GROUP_ID> zu laden..."
-oc rsh "$RAILS_POD" rails runner "require 'pp'; result = Group.find_by_id($SOURCE_GROUP_ID).name_last; pp result if result" > sg_output.txt || { echo "❌ Konnte Sourcegroup <$SOURCE_GROUP_ID> nicht laden."; exit 1; }
+echo "Trying to load name of sourcegroup <$SOURCE_GROUP_ID>..."
+oc rsh "$RAILS_POD" rails runner "require 'pp'; result = Group.find_by_id($SOURCE_GROUP_ID).name_last; pp result if result" > sg_output.txt || { echo "❌ Couldn't load sourcegroup <$SOURCE_GROUP_ID>."; exit 1; }
 SOURCE_GROUP_NAME=$(grep -v '^I' sg_output.txt | cut -d "\"" -f 2)
 rm sg_output.txt
-echo "Versuche Name von Targetgroup <$TARGET_GROUP_ID> zu laden..."
-oc rsh "$RAILS_POD" rails runner "require 'pp'; result = Group.find_by_id($TARGET_GROUP_ID).name_last; pp result if result" > tg_output.txt || { echo "❌ Konnte Targetgroup <$TARGET_GROUP_ID> nicht laden."; exit 1; }
+echo "Trying to load name of targetgroup <$TARGET_GROUP_ID>..."
+oc rsh "$RAILS_POD" rails runner "require 'pp'; result = Group.find_by_id($TARGET_GROUP_ID).name_last; pp result if result" > tg_output.txt || { echo "❌ Couldn't load targetgroup <$TARGET_GROUP_ID>."; exit 1; }
 TARGET_GROUP_NAME=$(grep -v '^I' tg_output.txt | cut -d "\"" -f 2)
 rm tg_output.txt
 
@@ -69,4 +69,4 @@ python cac-trigger-replacer.py --input trigger.ruby --output trigger_cac_ready.r
 
 rm trigger.ruby
 
-echo "✅ Export erfolgreich. Ergebnis --> trigger_cac_ready.ruby"
+echo "✅ Export completed successfully. Result --> trigger_cac_ready.ruby"
